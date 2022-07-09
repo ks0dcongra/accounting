cf62d2736cde499c2d44cbca78239efc6193b2a6
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-10 02:33:37 +0800

    feat: rename 'account to 'record'

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,18 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-const Account = require('../../models/account')
+const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Account.find({ userId })
+  Record.find({ userId })
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ _id: 'asc' })
-    .then(accounts => res.render('index', { accounts })) // 將資料傳給 index 樣板
+    .then(records => {
+      // console.log(records)
+      res.render('index', { records })
+    }) // 將資料傳給 index 樣板
+
     .catch(error => console.error(error)) // 錯誤處理
 })
 
