9bf728e1bc66253094a2607bf29c5e5ad077517e
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-09 22:17:48 +0800

    feat: add price attribute in account.s

--- models/account.js
+++ models/account.js
@@ -14,6 +14,10 @@ const accountSchema = new Schema({
     enum: ['home', 'transport', 'entertainment', 'food', 'other'],
     required: true
   },
+  price: {
+    type: String,
+    required: true
+  },
   userId: {  // 加入關聯設定
     type: Schema.Types.ObjectId,
     ref: 'User',
