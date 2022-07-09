0ee5a849492e3f523d831d00fc2dca7de12a2874
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-09 22:15:04 +0800

    feat: change accountSeeder to adapt account

--- models/account.js
+++ models/account.js
@@ -5,6 +5,15 @@ const accountSchema = new Schema({
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  class: {
+    type: String,
+    enum: ['home', 'transport', 'entertainment', 'food', 'other'],
+    required: true
+  },
   userId: {  // 加入關聯設定
     type: Schema.Types.ObjectId,
     ref: 'User',
