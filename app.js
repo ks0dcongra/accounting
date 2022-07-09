54f882d8266410af9ffba37b56c0bfcd1b7ea005
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-09 16:19:58 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Account.find()
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' })
     .then(accounts => res.render('index', { accounts })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
