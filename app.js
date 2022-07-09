62f83c449fcbe424d7de266c94c8296e0fdac11b
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-09 15:05:21 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
+const Accounting = require('./models/accounting') // 載入 Todo model
 const exphbs = require('express-handlebars');
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -23,7 +23,12 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-
+app.get('/', (req, res) => {
+  Accounting.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { accounting })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
