7344bd8a8f06ecbd28e22942fff669175550deae
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-09 16:03:58 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const Account = require('./models/account')
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -25,6 +28,17 @@ app.get('/', (req, res) => {
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(accounts => res.render('index', { accounts })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/accounts/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/accounts', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Account.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
