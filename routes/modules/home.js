f590efe92c91949b46aba3859864fa318ece0115
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-10 22:23:56 +0800

    feat: add category display feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,24 +3,47 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const handlebars = require('handlebars')
+handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
 
 router.get('/', async (req, res) => {
   try {
+    // const categories = await Category.find().lean().sort('_id')
     const userId = req.user._id
     const records = await Record.find({ userId }).lean()
-    const categories = await Category.find().lean()
+    const categories = await Category.find().lean().sort()
     let totalAmount = 0
-
     records.forEach(record => {
       totalAmount += record.amount
       categories.find(data => {
         if (data._id.toString().includes(record.categoryId.toString())) {
           record.icon = data.icon
-          console.log(record.icon)
         }
       })
     })
-    res.render('index', { records, totalAmount })
+    res.render('index', { records, totalAmount, categories })
+  }
+  catch (error) {
+    console.log(error.message)
+  }
+})
+
+router.get('/:categoryId', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const categoryId = req.params.categoryId
+    const records = await Record.find({ userId, categoryId }).lean()
+    const categories = await Category.find().lean()
+    let totalAmount = 0
+    records.forEach(record => {
+      totalAmount += record.amount
+      categories.find(data => {
+        if (data._id.toString().includes(record.categoryId.toString())) {
+          record.icon = data.icon
+        }
+      })
+    })
+    res.render('index', { records, totalAmount, categories })
   }
   catch (error) {
     console.log(error.message)
