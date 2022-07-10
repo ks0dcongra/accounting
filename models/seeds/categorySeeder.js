4923e7202882b69b96f9d60506389c9c19152fc6
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-10 21:52:16 +0800

    feat: revise icon class

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,12 +5,13 @@ if (process.env.NODE_ENV !== 'production') {
 const db = require('../../config/mongoose')
 
 const categoryList = [
-  { name: '家居物業', icon: 'fa-house' },
-  { name: '交通出行', icon: 'fa-van-shuttle' },
-  { name: '休閒娛樂', icon: 'fa-face-grin-beam' },
-  { name: '餐飲食品', icon: 'fa-utensils' },
-  { name: '其他', icon: 'fa-pen' }
+  { name: '家居物業', icon: 'fa-solid fa-house' },
+  { name: '交通出行', icon: 'fa-solid fa-van-shuttle' },
+  { name: '休閒娛樂', icon: 'fa-solid fa-face-grin-beam' },
+  { name: '餐飲食品', icon: 'fa-solid fa-utensils' },
+  { name: '其他', icon: 'fa-solid fa-pen' }
 ]
+
 
 db.once('open', () => {
   Promise.all(Array.from(categoryList, category => {
