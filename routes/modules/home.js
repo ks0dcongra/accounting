58fe32db1c68ad6a87aead8ca4550b91c2be73de
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-10 17:06:06 +0800

    refactor: code more clear

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,11 +9,10 @@ router.get('/', (req, res) => {
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ _id: 'asc' })
     .then(records => {
-      // console.log(records)
       res.render('index', { records })
     }) // 將資料傳給 index 樣板
 
-    .catch(error => console.error(error)) // 錯誤處理
+    .catch(error => console.log(error)) // 錯誤處理
 })
 
 module.exports = router
